a8c5eeb93b3d8c4faa405c07a8f228c27dea2ce6
Sanches <bulanov.shura@gmail.com>
2021-12-05 09:25:26 +0300

    Refactoring Cod

--- Assets/Scripts/House.cs
+++ Assets/Scripts/House.cs
@@ -7,51 +7,40 @@ using UnityEngine.Events;
 
 public class House : MonoBehaviour
 {
-    [SerializeField] private bool _activatingSiren = true;
-
-    private event UnityAction _play;
-    private event UnityAction _stop;
-    private IEnumerator _coroutine;
+    private IEnumerator _increaseVolume;
+    private IEnumerator _decreaseVolume;
     private AudioSource _sound;
     private HouseExterior _houseExterior;
     private HouseInterior _houseInterior;
-    private float _maxSirenVolume = 1f;
-    private float _minSirenVolume = 0.3f;
-    private float _changeVolumeRate = 0.7f;
-    private int _inversingVariable;
 
     private void Start()
     {
-        _coroutine = Louder();
+        _increaseVolume = Louder(1);
+        _decreaseVolume = Louder(-1);
+        _sound = GetComponent<AudioSource>();
+        _sound.volume = 0;
         _houseExterior = GetComponentInChildren<HouseExterior>();
         _houseInterior = GetComponentInChildren<HouseInterior>();
-        _sound = GetComponent<AudioSource>();
-        _sound.volume = _minSirenVolume;
         _houseInterior.gameObject.SetActive(false);
         _houseExterior.gameObject.SetActive(true);
-        _play = _sound.Play;
-        _stop = _sound.Stop;
+
     }
 
-    private IEnumerator Louder()
+    private IEnumerator Louder(float changeValueVariable)
     {
-        while (_activatingSiren)
+
+        while (_sound.volume < 1 | _sound.volume > 0)
         {
-            if (_sound.volume <= _minSirenVolume)
-            {
-                _inversingVariable = 1;
-            }
 
-            if (_sound.volume >= _maxSirenVolume)
-            {
-                _inversingVariable = -1;
-            }
+            _sound.volume = Mathf.MoveTowards(_sound.volume, 1f, 0.5f * changeValueVariable * Time.deltaTime) ;
 
-            _sound.volume = Mathf.MoveTowards(_sound.volume, _maxSirenVolume,
-
-            _changeVolumeRate *_inversingVariable* Time.deltaTime);
-                       
             yield return null;
+
+            if (_sound.volume <= 0)
+            {
+                _sound.Stop();
+            }
+
         }
     }
 
@@ -59,15 +48,17 @@ public class House : MonoBehaviour
     {
         if (collision.TryGetComponent<Robber>(out Robber robber))
         {
-            _activatingSiren = true;
 
             _houseExterior.gameObject.SetActive(false);
 
             _houseInterior.gameObject.SetActive(true);
 
-            StartCoroutine(_coroutine);
+            StopCoroutine(_decreaseVolume);
 
-            _play?.Invoke();
+            StartCoroutine(_increaseVolume);
+
+            _sound.Play();
+
         }
     }
 
@@ -75,19 +66,15 @@ public class House : MonoBehaviour
     {
         if (collision.TryGetComponent<Robber>(out Robber robber))
         {
-            _activatingSiren = false;
 
             _houseExterior.gameObject.SetActive(true);
 
             _houseInterior.gameObject.SetActive(false);
 
-            _stop?.Invoke();
+            StopCoroutine(_increaseVolume);
 
-            StopCoroutine(_coroutine);
+            StartCoroutine(_decreaseVolume);
 
-            _sound.volume = _minSirenVolume;
-
-            _inversingVariable = 1;
         }
     }
 }
